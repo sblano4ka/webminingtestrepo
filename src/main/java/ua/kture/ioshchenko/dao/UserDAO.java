b7aad89aa76c5aa233ebf34e6f9ab72a88b9c01d
Ioshchenko Ivan <vanya88@ukr.net>
2014-05-14 21:40:42 +0300

    Commit from JBoss Tools

--- src/main/java/ua/kture/ioshchenko/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package ua.kture.ioshchenko.dao;
-
-import ua.kture.ioshchenko.model.User;
-
-public interface UserDAO {
-    public User add(User user);
-
-    public User get(String email, String password);
-}
--- src/main/java/ua/kture/ioshchenko/service/UserService.java
+++ src/main/java/ua/kture/ioshchenko/service/UserService.java
@@ -2,14 +2,16 @@ package ua.kture.ioshchenko.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+/*
 import org.springframework.transaction.annotation.Transactional;
 import ua.kture.ioshchenko.dao.UserDAO;
 import ua.kture.ioshchenko.model.User;
+*/
 
 @Service
 public class UserService {
 
-    @Autowired
+    /*@Autowired
     private UserDAO userDAO;
 
     @Transactional
@@ -20,5 +22,5 @@ public class UserService {
     @Transactional
     public User get(String email, String password) {
         return userDAO.get(email, password);
-    }
+    }*/
 }
