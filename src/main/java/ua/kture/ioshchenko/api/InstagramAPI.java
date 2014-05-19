4d3f05e08b16a79522b8dce16e71b13b6cc191db
Ioshchenko Ivan <vanya88@ukr.net>
2014-05-19 21:41:15 +0300

    Commit from JBoss Tools

--- src/main/java/ua/kture/ioshchenko/api/InstagramAPI.java
+++ src/main/java/ua/kture/ioshchenko/api/InstagramAPI.java
@@ -22,68 +22,67 @@ import java.util.List;
 
 @Component
 public class InstagramAPI {
-    private static final String CLENT_ID = "883cf7e7219848a59b1b0fe295ddf549";
-    private static final String CLIENT_SECRET = "9d0585ac55e146e1877e5c1b694ffb64";
-    private static final String REDIRECT_URL = "http://fake-ioschenko.rhcloud.com/instagram/accesss";
+	private static final String CLENT_ID = "883cf7e7219848a59b1b0fe295ddf549";
+	private static final String CLIENT_SECRET = "9d0585ac55e146e1877e5c1b694ffb64";
+	private static final String REDIRECT_URL = "http://fake-ioschenko.rhcloud.com/instagram/accesss";
 
-    private String authUrl;
+	private String authUrl;
 
+	private Logger log = Logger.getLogger(InstagramAPI.class);
 
-    private Logger log = Logger.getLogger(InstagramAPI.class) ;
+	public InstagramAPI() {
+		StringBuilder url = new StringBuilder(
+				"https://api.instagram.com/oauth/authorize/?client_id=");
+		url.append(CLENT_ID).append("&redirect_uri=").append(REDIRECT_URL)
+				.append("&response_type=code");
+		authUrl = url.toString();
+	}
 
-    public InstagramAPI() {
-        StringBuilder url = new StringBuilder("https://api.instagram.com/oauth/authorize/?client_id=");
-        url.append(CLENT_ID)
-                .append("&redirect_uri=").append(REDIRECT_URL)
-                .append("&response_type=code");
-        authUrl = url.toString();
-    }
+	public String getAccessToken(String code) throws IOException {
+		log.info("**************  Start API  **************");
+		HttpPost httpPost = null;
+		StringBuilder builder = new StringBuilder();
 
-    public String getAccessToken(String code) throws IOException {
-        log.info("**************  Start API  **************");
-        HttpPost httpPost = null;
-        StringBuilder builder = new StringBuilder();
+		try {
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			httpPost = new HttpPost(
+					"https://api.instagram.com/oauth/access_token");
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-        try {
-            DefaultHttpClient httpclient = new DefaultHttpClient();
-            httpPost = new HttpPost("https://api.instagram.com/oauth/access_token");
-            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			nvps.add(new BasicNameValuePair("client_id", CLENT_ID));
+			nvps.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
+			nvps.add(new BasicNameValuePair("grant_type", "authorization_code"));
+			nvps.add(new BasicNameValuePair("redirect_uri", REDIRECT_URL));
+			nvps.add(new BasicNameValuePair("code", code));
 
-            nvps.add(new BasicNameValuePair("client_id", CLENT_ID));
-            nvps.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
-            nvps.add(new BasicNameValuePair("grant_type", "authorization_code"));
-            nvps.add(new BasicNameValuePair("redirect_uri", REDIRECT_URL));
-            nvps.add(new BasicNameValuePair("code", code));
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+			HttpResponse response = httpclient.execute(httpPost);
 
-            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-            HttpResponse response = httpclient.execute(httpPost);
-           
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent(), "UTF-8"));
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-           
-            for (String line = null; (line = reader.readLine()) != null; ) {
-                builder.append(line).append("\n");
-            }
+			for (String line = null; (line = reader.readLine()) != null;) {
+				builder.append(line);
+			}
 
-            //System.out.println(builder.toString());
+			// System.out.println(builder.toString());
 
-            //json = (JSONObject) new JSONParser().parse(builder.toString());
-            
-            EntityUtils.consume(response.getEntity());
+			// json = (JSONObject) new JSONParser().parse(builder.toString());
 
-        } catch (Exception ex) {
-                log.error("Instagram api", ex);
-        } finally {
-            httpPost.releaseConnection();
+			EntityUtils.consume(response.getEntity());
 
-        }
+		} catch (Exception ex) {
+			log.error("Instagram api", ex);
+		} finally {
+			httpPost.releaseConnection();
 
-        return builder.toString();
+		}
 
-    }
+		return builder.toString();
 
+	}
 
-    public String getAuthUrl() {
-        return authUrl;
-    }
+	public String getAuthUrl() {
+		return authUrl;
+	}
 }
